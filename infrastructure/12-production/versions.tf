e4570ea17a0a9eb58f5d947559010282dcb9dd13
voytenkov <voytenkov@inbox.ru>
2023-10-07 03:53:50 +0200

    terraform updates

---
+++ infrastructure/12-production/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.1.6"
+  
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.87.0"
+    }
+  }
+
+  backend "http" { 
+  }
+}
