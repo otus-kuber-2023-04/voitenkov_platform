f7bf7d5294bc9361e05c31993f5f205428337c10
voytenkov <voytenkov@inbox.ru>
2023-10-26 15:48:57 +0200

    repo content was rearranged

--- infrastructure/2-cloud/main.tf
+++
@@ -1,5 +0,0 @@
-module "dev-folder" {
-    source             = "../modules/folder"
-    folder_project     = "otus-kuber"
-    folder_environment = "dev" 
-} 
--- infrastructure/modules/folder/otputs.tf
+++
@@ -1,9 +0,0 @@
-output "tfstate_access_key" {
-  value = yandex_iam_service_account_static_access_key.sa-tfstate-static-key.access_key
-  sensitive = true
-}
-
-output "tfstate_secret_key" {
-  value = yandex_iam_service_account_static_access_key.sa-tfstate-static-key.secret_key
-  sensitive = true
-}
